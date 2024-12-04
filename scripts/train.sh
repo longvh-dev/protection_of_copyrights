1006d1be1d357d3862d9f147b3afb0ac0141bfca
longvh <longvh.work@gmail.com>
2024-12-04 23:44:58 +0700

    add slurm script

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 # nohup python -u tools/train.py > logs.log &
-nohup python -u train.py \
-    --num_epochs 100 \
-    --batch_size 4 \
-    --lr 0.001 \
-    --alpha 1.0 \
-    --beta 0.0 \
-    --watermark_region 4.0 \
-    --train_dir "data/wikiart" \
-    --train_classes "data/wikiart/train_classes.csv" \
-    --eval_dir "data/wikiart" \
-    --eval_classes "data/wikiart/eval_classes.csv" \
-    > logs.log &
+# nohup python -u train.py \
+#     --num_epochs 100 \
+#     --batch_size 4 \
+#     --lr 0.001 \
+#     --alpha 1.0 \
+#     --beta 0.0 \
+#     --watermark_region 4.0 \
+#     --train_dir "data/wikiart" \
+#     --train_classes "data/wikiart/train_classes.csv" \
+#     --eval_dir "data/wikiart" \
+#     --eval_classes "data/wikiart/eval_classes.csv" \
+#     > logs.log &
+
+sbatch train_model.slurm
