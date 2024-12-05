6f80c082576957fc069ae46bc756d5dde89004c8
longvh <longvh.work@gmail.com>
2024-12-05 08:39:11 +0700

    feat: improve L2 normalization and update adversarial loss calculations

--- losses/__init__.py
+++ losses/__init__.py
@@ -10,7 +10,7 @@ __all__ = [
     'perturbation_loss'
 ]
 
-def l2norm(X, dim, eps=1e-8):
+def l2norm(X, dim=-1, eps=1e-8):
     """L2-normalize columns of X
     """
     norm = torch.pow(X, 2).sum(dim=dim, keepdim=True).sqrt() + eps
@@ -24,18 +24,18 @@ def gan_loss(D, real_images, fake_images):
 
 
 def adversarial_loss(vae, x_prime, watermark):
-    latent_watermark = vae.encode(watermark)
-    latent_adversarial = vae.encode(x_prime)
-    return (l2norm(latent_adversarial - latent_watermark, dim=1)**2).mean()
+    latent_watermark = l2norm(vae.encode(watermark))
+    latent_adversarial = l2norm(vae.encode(x_prime))
+    return torch.norm(latent_watermark-latent_adversarial, p=2, dim=1)
     # return F.mse_loss(latent_adversarial, latent_watermark) 
 
 
 def perturbation_loss(perturbation, watermark, c=0.1, watermark_region=4):
     weighted_perturbation = torch.matmul(perturbation, (1 + watermark * watermark_region))
     # l2_norm = torch.norm(weighted_perturbation, p=2)
-    l2_norm = l2_norm(weighted_perturbation, dim=1)
-    mask = ((l2_norm - c)>0) + 0 
-    threshold_loss = (l2_norm * mask).mean()
+    weighted_perturbation = l2norm(weighted_perturbation, dim=1)
+    mask = ((weighted_perturbation - c)>0) + 0 
+    threshold_loss = (weighted_perturbation * mask).mean()
     # threshold_loss = torch.max(torch.zeros_like(l2_norm), l2_norm - c)
     
     return threshold_loss # + small_perturbation_penalty
